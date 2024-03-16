5d2a69ce95b6755e770a38c993c802c798ff7b4c
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-16 10:42:10 +0700

    feat: header section

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template lang="">
+  <div class="container">
+    <img src="../assets/radwahpadang-logo.svg" class="max-w-[558px] w-full mx-auto" alt="">
+    <ul class="category-list text-primary flex justify-center items-center flex-wrap gap-10 p-5">
+      <li>Dress</li>
+      <li>Tunik</li>
+      <li>Shirt</li>
+      <li>Pants</li>
+      <li>Outer</li>
+      <li>Bag</li>
+      <li>Scarf</li>
+      <li>Prayer set</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
