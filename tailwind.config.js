7555ed6d949b0fb56d245b9f89432247a29bee94
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-16 10:41:58 +0700

    first commit and tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': "#CA2D0E"
+      }
+    },
+  },
+  plugins: [],
+}
+
