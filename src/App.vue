9783a936a0716b4118fdc7dba16d7458141020d2
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-17 11:45:42 +0700

    feat: testimonial section

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Header from './components/Header.vue';
 import Hero from './components/Hero.vue';
 import RunningText from './components/RunningText.vue';
+import Testimonial from './components/Testimonial.vue';
 </script>
 
 <template>
   <Header />
   <Hero />
   <RunningText />
+  <Testimonial />
 </template>
 
 <style scoped>
---
+++ src/components/Testimonial.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container my-10">
+      <div class="text-center mb-20">
+        <span class="py-1 px-4 bg-secondary text-white rounded-md inline-block">Trusted</span>
+        <h2 class="mt-3">Ulasan dari Pelanggan</h2>
+        <p class="text-xl">Pesanan anda kami pastikan sesuai dan sampai dengan selamat</p>
+      </div>
+      <div class="grid grid-cols-4 gap-8 my-10">
+        <img src="/images/testi-1.png" alt="">
+        <img src="/images/testi-2.png" alt="">
+        <img src="/images/testi-1.png" alt="">
+        <img src="/images/testi-2.png" alt="">
+      </div>
+  </section>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  
+</style>
