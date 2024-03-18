8931f656d5cb3bd99de6baaf23e0b847a9845ae6
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-18 11:12:58 +0700

    feat: improve CTA section

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,8 +1,11 @@
 <template>
-  <section class="container my-[50px]">
+  <section class="container my-[150px]">
     <div class="grid grid-cols-3 gap-10 mx-20">
       <div class="col-span-2 bg-black text-white p-10 rounded-2xl flex flex-col justify-between">
-        <h2 class="leading-[1] mb-5 text-[48px] font-semibold">Cek katalog lengkap di Instagram kami</h2>
+        <div class="relative">
+          <h2 class="leading-[1] mb-5 text-[48px] font-semibold pl-[20px]">Cek katalog lengkap di Instagram kami</h2>
+          <img src="/icons/ic-highlight.svg" width="24" alt="" class="absolute left-0 -top-4 animate-bounce">
+        </div>
         <div class="flex justify-between items-end">
           <ButtonCTA :title="title"/>
           <div class="flex items-center gap-2 text-white">
@@ -11,11 +14,14 @@
           </div>
         </div>
       </div>
-      <div class="bg-white p-10 rounded-2xl space-y-6 relative">
+      <div class="bg-white p-10 rounded-2xl space-y-6 relative group hover:animate-flip transition-all">
         <img src="/icons/ic-whatsapp-black.svg" width="80" alt="" fill="black">
-        <h3 class="text-[32px] font-semibold">Fast Respon</h3>
+        <h3 class="text-[24px] font-semibold">Fast Respon</h3>
         <p class="text-graytext">Jika ada pertanyaan atau pemesanan, langsung hubungi admin kami melalui WhatsApp ya.</p>
         <img src="/icons/ic-arrow-up.svg" alt="" class="absolute top-0 right-[20px]">
+        <div class="absolute inset-0 bg-white/30 backdrop-blur-sm rounded-2xl justify-center items-center hidden group-hover:flex">
+          <a href="https://wa.me/082288794956" target="_blank" class="text-md uppercase font-bold inline-block bg-white py-2 px-6 rounded-3xl border-2 border-black hover:bg-black hover:text-white hover:border-black">Chat Admin</a>
+        </div>
       </div>
     </div>
   </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,14 @@ export default {
         'primaryLight': "#FFD4CC",
         'secondary': "#007475",
         'graytext': "#656565"
+      },
+      keyframes: {
+        flip: {
+          '0%': {'transform' : 'rotateY(180deg)'}
+        }
+      },
+      animation: {
+        flip: 'flip 1s ease'
       }
     },
   },
