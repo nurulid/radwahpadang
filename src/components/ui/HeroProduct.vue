5a40f2244f138f6d9bdd5f93ce64deffd5683fb8
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-19 13:45:00 +0700

    fix: improve hero products

--- src/components/ui/HeroProduct.vue
+++ src/components/ui/HeroProduct.vue
@@ -1,8 +1,8 @@
 <template lang="">
-  <div v-for="({id, image, name, url, sale}) in data" :key="id" class="relative" data-aos="zoom-in" data-aos-duration="500">
-    <img class="rounded-xl h-full" :src="image" alt="">
-    <div class="absolute bottom-0 left-0 right-0 bg-white/50 backdrop-blur-sm px-5 py-3 rounded-xl m-1 flex justify-between group">
-      <h3 class="text-left text-sm md:text-base font-semibold uppercase">{{name}}</h3>
+  <div v-for="({id, image, name, url, sale}) in data" :key="id" data-aos="zoom-in" data-aos-duration="500" class="bg-white p-3 flex flex-col justify-between group">
+    <img class="rounded-xl h-full group-hover:brightness-[1.1] transition-all" :src="image" alt="">
+    <div class=" bg-white mt-4 flex justify-between ">
+      <h3 class="text-left text-xs md:text-sm font-semibold uppercase group-hover:text-primary transition-all">{{name}}</h3>
       <MoveUpRight class="group-hover:rotate-45 transition-all"/>
       <a :href="url" target="_blank" class="absolute inset-0"></a>
     </div>
