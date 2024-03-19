1cc9b6b2931d41c40b4921bff0584d4df5dacd2a
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-19 15:19:56 +0700

    updated

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template lang="">
   <footer>
-    <div class="inner container !px-6 lg:!px-0 !py-6 flex justify-between items-center flex-col md:flex-row gap-2 border-t border-graytext text-graytext text-xs">
+    <div class="inner container !px-6 lg:!px-0 !py-6 flex justify-between items-center flex-col md:flex-row gap-3 border-t border-graytext text-graytext text-xs">
         <p class="flex items-center">
           <span class="pr-4">@ 2024</span> 
-          <a href="#" class="inline-flex gap-2 items-center pl-4 border-l border-graytext"><Instagram size="18"/> radwahpadang</a>
+          <a href="https://www.instagram.com/radwahpadang" target="_blank" class="inline-flex gap-2 items-center pl-4 border-l border-graytext"><Instagram size="18"/> radwahpadang</a>
         </p>
         <a href="https://nurulid.space" target="_blank">Site by Nurul Insani Dewi</a>
     </div>
--- src/components/ui/HeroProduct.vue
+++ src/components/ui/HeroProduct.vue
@@ -1,6 +1,6 @@
 <template lang="">
-  <div v-for="({id, image, name, url, sale}) in data" :key="id" data-aos="zoom-in" data-aos-duration="500" class="bg-white p-3 flex flex-col justify-between group">
-    <img class="rounded-xl h-full group-hover:brightness-[1.1] transition-all" :src="image" alt="">
+  <div v-for="({id, image, name, url, sale}) in data" :key="id" data-aos="zoom-in" data-aos-duration="500" class="bg-white p-3 flex flex-col justify-between group rounded">
+    <img class="rounded h-full group-hover:brightness-[1.1] transition-all" :src="image" alt="">
     <div class=" bg-white mt-4 flex justify-between ">
       <h3 class="text-left text-xs md:text-sm font-semibold uppercase group-hover:text-primary transition-all">{{name}}</h3>
       <MoveUpRight class="group-hover:rotate-45 transition-all"/>
