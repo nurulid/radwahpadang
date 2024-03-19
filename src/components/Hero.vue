3b210ccc32004e838a0f6dc9125647fef93b11cd
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-19 12:17:57 +0700

    fix: modular component hero products

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,31 +1,72 @@
 <template>
   <section class="container mb-10 text-center">
-    <div class="grid grid-cols-3 gap-10 mb-20">
-      <img class="rounded-xl" data-aos="zoom-in" data-aos-duration="500" src="/images/bag.png" alt="">
-      <img class="rounded-xl mt-10" data-aos="zoom-in" data-aos-duration="1000" src="/images/pants.png" alt="">
-      <img class="rounded-xl" data-aos="zoom-in" data-aos-duration="1500" src="/images/tunik.png" alt="">
-      <img class="rounded-xl" data-aos="zoom-in" data-aos-duration="2000" src="/images/mukena.png" alt="">
-      <img class="rounded-xl mt-10" data-aos="zoom-in" data-aos-duration="2500" src="/images/scarf.png" alt="">
-      <img class="rounded-xl" data-aos="zoom-in" data-aos-duration="3000" src="/images/outer.png" alt="">
+    <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 md::gap-10 mb-20">
+      <HeroProduct :data="products"/>
     </div>
-    <div data-aos="fade-right" data-aos-delay="100"  data-aos-offset="800" >
+    <div data-aos="fade-right" data-aos-delay="300"  data-aos-offset="100" >
       <ButtonCTA :title="title" />
     </div>
   </section>
 </template>
 <script lang="js">
-import ButtonCTA from './ui/ButtonCTA.vue'
+import ButtonCTA from './ui/ButtonCTA.vue';
+import HeroProduct from './ui/HeroProduct.vue';
 
 export default {
   name: 'Hero',
   components: {
-    ButtonCTA
+    ButtonCTA,
+    HeroProduct,
   },
   data() {
     return {
       title: 'Cek Katalog Lengkap',
+      products: [
+        {
+          id: '01',
+          image: '/images/bag.png',
+          name: 'Jasper Bag Teracotta',
+          url: 'https://www.instagram.com/p/Cpj4UBNJxTX/?img_index=1',
+          sale: true
+        },
+        {
+          id: '02',
+          image: '/images/pants.png',
+          name: 'Reeva Pants',
+          url: 'https://www.instagram.com/p/C3mjOJ2pEJK/?img_index=1',
+          sale: true
+        },
+        {
+          id: '03',
+          image: '/images/tunik.png',
+          name: 'Bahriya Tunic',
+          url: 'https://www.instagram.com/p/CpsH7KwJmoe/?img_index=1',
+          sale: true
+        },
+        {
+          id: '04',
+          image: '/images/mukena.png',
+          name: 'Premium Jasmine Prayer Set',
+          url: 'https://www.instagram.com/p/Cqj9kFjJTYQ/?img_index=4',
+          sale: true
+        },
+        {
+          id: '05',
+          image: '/images/scarf.png',
+          name: 'Azalena Scarf',
+          url: 'https://www.instagram.com/p/CrAQKB-pRdt/?img_index=1',
+          sale: true
+        },
+        {
+          id: '06',
+          image: '/images/outer.png',
+          name: 'Tiana Outer',
+          url: 'https://www.instagram.com/p/C3l3zNnJABg/?img_index=1',
+          sale: true
+        }
+      ]
     };
   },
 }
 </script>
-<style></style>./ui/ButtonPrimary.vue./ui/ButtonCTA.vue
+<style></style>./ui/ButtonPrimary.vue./ui/ButtonCTA.vue./ui/HeroProduct.vue
---
+++ src/components/ui/HeroProduct.vue
@@ -0,0 +1,27 @@
+<template lang="">
+  <div v-for="({id, image, name, url, sale}) in data" :key="id" class="relative" data-aos="zoom-in" data-aos-duration="500">
+    <img class="rounded-xl h-full" :src="image" alt="">
+    <div class="absolute bottom-0 left-0 right-0 bg-white/50 backdrop-blur-sm px-5 py-3 rounded-xl m-1 flex justify-between group">
+      <h3 class="text-left text-sm md:text-base font-semibold uppercase">{{name}}</h3>
+      <MoveUpRight class="group-hover:rotate-45 transition-all"/>
+      <a :href="url" target="_blank" class="absolute inset-0"></a>
+    </div>
+  </div>
+</template>
+<script>
+import { MoveUpRight } from 'lucide-vue-next';
+
+export default {
+  name: "HeroProduct",
+  components: {
+    MoveUpRight
+  },
+  props: {
+    data: Array
+  }
+  
+}
+</script>
+<style lang="">
+  
+</style>
