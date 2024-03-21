3ef130d5cd3a399c885f1ff8cf7936485c2fb5ed
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-21 10:34:21 +0700

    tried vue-meta for SEO

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from "vue";
+import App from "./App.vue";
+import Meta from "vue-meta";
 import "./css/style.css";
 import "./css/global.css";
-import App from "./App.vue";
 
 // importing AOS
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-createApp(App)
-  .use(AOS.init())
-  .mount("#app");
+createApp(App).use(AOS.init(), Meta).mount("#app");
