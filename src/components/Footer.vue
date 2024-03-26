3a37a75a22f53ebfd76733b71fb8702c0bae2e88
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-03-26 11:38:20 +0700

    fix: tweak footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <footer>
-    <div class="inner container-custom !px-6 lg:!px-0 !py-6 flex justify-between items-center flex-col md:flex-row gap-3 border-t border-graytext text-graytext text-xs">
+    <div class="inner container-custom !px-6 lg:!px-4 !py-6 flex justify-between items-center flex-col md:flex-row gap-3 border-t border-graytext text-graytext text-xs">
         <p class="flex items-center">
           <span class="pr-4">@ 2024</span> 
           <a href="https://www.instagram.com/radwahpadang" target="_blank" class="inline-flex gap-2 items-center pl-4 border-l border-graytext"><Instagram size="18"/> radwahpadang</a>
