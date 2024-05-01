7b7841b2f7f5e57b14de3ffc10bd93189b48538f
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-05-01 21:18:33 +0700

    get data from notion api

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="container-custom mb-10 text-center">
     <div class="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-5 md::gap-10 mb-20">
-      <HeroProduct :data="products"/>
+      <HeroProduct :data="data"/>
     </div>
     <div data-aos="fade-right" data-aos-delay="300"  data-aos-offset="100" >
       <ButtonCTA :title="title" :url="url"/>
@@ -9,6 +9,9 @@
   </section>
 </template>
 <script lang="js">
+import axios from 'axios';
+
+// Components
 import ButtonCTA from './ui/ButtonCTA.vue';
 import HeroProduct from './ui/HeroProduct.vue';
 
@@ -22,52 +25,69 @@ export default {
     return {
       title: 'Cek Katalog Lengkap',
       url: 'https://www.instagram.com/radwahpadang/',
-      products: [
-        {
-          id: '01',
-          image: './images/bag.png',
-          name: 'Jasper Bag Teracotta',
-          url: 'https://www.instagram.com/p/Cpj4UBNJxTX/?img_index=1',
-          sale: true
-        },
-        {
-          id: '02',
-          image: './images/pants.png',
-          name: 'Reeva Pants',
-          url: 'https://www.instagram.com/p/C3mjOJ2pEJK/?img_index=1',
-          sale: false
-        },
-        {
-          id: '03',
-          image: './images/tunik.png',
-          name: 'Bahriya Tunic',
-          url: 'https://www.instagram.com/p/CpsH7KwJmoe/?img_index=1',
-          sale: false
-        },
-        {
-          id: '04',
-          image: './images/mukena.png',
-          name: 'Premium Jasmine Prayer Set',
-          url: 'https://www.instagram.com/p/Cqj9kFjJTYQ/?img_index=4',
-          sale: false
-        },
-        {
-          id: '05',
-          image: './images/scarf.png',
-          name: 'Azalena Scarf',
-          url: 'https://www.instagram.com/p/CrAQKB-pRdt/?img_index=1',
-          sale: false
-        },
-        {
-          id: '06',
-          image: './images/outer.png',
-          name: 'Tiana Outer',
-          url: 'https://www.instagram.com/p/C3l3zNnJABg/?img_index=1',
-          sale: false
-        }
-      ]
+      data: [],
+      // products: [
+      //   {
+      //     id: '01',
+      //     image: './images/bag.png',
+      //     name: 'Jasper Bag Teracotta',
+      //     url: 'https://www.instagram.com/p/Cpj4UBNJxTX/?img_index=1',
+      //     sale: true
+      //   },
+      //   {
+      //     id: '02',
+      //     image: './images/pants.png',
+      //     name: 'Reeva Pants',
+      //     url: 'https://www.instagram.com/p/C3mjOJ2pEJK/?img_index=1',
+      //     sale: false
+      //   },
+      //   {
+      //     id: '03',
+      //     image: './images/tunik.png',
+      //     name: 'Bahriya Tunic',
+      //     url: 'https://www.instagram.com/p/CpsH7KwJmoe/?img_index=1',
+      //     sale: false
+      //   },
+      //   {
+      //     id: '04',
+      //     image: './images/mukena.png',
+      //     name: 'Premium Jasmine Prayer Set',
+      //     url: 'https://www.instagram.com/p/Cqj9kFjJTYQ/?img_index=4',
+      //     sale: false
+      //   },
+      //   {
+      //     id: '05',
+      //     image: './images/scarf.png',
+      //     name: 'Azalena Scarf',
+      //     url: 'https://www.instagram.com/p/CrAQKB-pRdt/?img_index=1',
+      //     sale: false
+      //   },
+      //   {
+      //     id: '06',
+      //     image: './images/outer.png',
+      //     name: 'Tiana Outer',
+      //     url: 'https://www.instagram.com/p/C3l3zNnJABg/?img_index=1',
+      //     sale: false
+      //   }
+      // ]
     };
+  },
+  mounted() {
+    this.fetchDataFromAPI();
+  },
+  methods: {
+    fetchDataFromAPI() {
+      // using notion-api-worker https://github.com/splitbee/notion-api-worker?tab=readme-ov-file
+      const apiUrl = 'https://notion-api.splitbee.io/v1/table/7286d14c38f94b26b3807b8d97810839';
+
+      axios.get(apiUrl)
+        .then((response) => {
+          this.data = response.data;
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    },
   },
 }
 </script>
-<style></style>./ui/ButtonPrimary.vue./ui/ButtonCTA.vue./ui/HeroProduct.vue
--- src/components/ui/HeroProduct.vue
+++ src/components/ui/HeroProduct.vue
@@ -1,12 +1,12 @@
 <template lang="">
-  <div v-for="({id, image, name, url, sale}) in data" :key="id" data-aos="zoom-in" data-aos-duration="500" class="bg-white p-3 flex flex-col justify-between group rounded-md relative" v-lazy-container="{ selector: 'img' }">
-    <img class="rounded h-full group-hover:brightness-[1.1] transition-all" :data-src="image" alt="">
+  <div v-for="({id, Image, Name, Link, Sale}) in data" :key="id" data-aos="zoom-in" data-aos-duration="500" class="bg-white p-3 flex flex-col justify-between group rounded-md relative" v-lazy-container="{ selector: 'img' }">
+    <img class="rounded aspect-square h-auto w-full object-cover group-hover:brightness-[1.1] transition-all" :data-src="Image[0].url" alt="">
     <div class=" bg-white mt-4 flex justify-between ">
-      <h3 class="text-left text-xs md:text-sm font-semibold uppercase group-hover:text-primary transition-all">{{name}}</h3>
+      <h3 class="text-left text-xs md:text-sm font-semibold uppercase group-hover:text-primary transition-all">{{Name}}</h3>
       <MoveUpRight class="group-hover:rotate-45 transition-all"/>
-      <a :href="url" target="_blank" class="absolute inset-0"></a>
+      <a :href="Link" target="_blank" class="absolute inset-0"></a>
     </div>
-    <span v-if="sale" class="absolute -top-3 -left-3 bg-primary rounded-full text-white text-xs md:text-[1rem] font-bold w-[2.2rem] md:w-[3rem] h-[2.2rem] md:h-[3rem] leading-[2.2rem] md:leading-[3rem] align-middle shadow-2xl">Sale</span>
+    <span v-if="Sale === 'Yes'" class="absolute -top-3 -left-3 bg-primary rounded-full text-white text-xs md:text-[1rem] font-bold w-[2.2rem] md:w-[3rem] h-[2.2rem] md:h-[3rem] leading-[2.2rem] md:leading-[3rem] align-middle shadow-2xl">Sale</span>
   </div>
 </template>
 <script>
